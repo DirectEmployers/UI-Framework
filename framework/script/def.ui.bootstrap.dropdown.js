68914d36159f066bd74c83ad9256bc8e01f44604
Jason Sole <jason@directemployers.org>
2011-11-04 16:02:02 -0400

    Add javascript library files and pointers

--- framework/script/def.ui.bootstrap.dropdown.js
+++ framework/script/def.ui.bootstrap.dropdown.js
@@ -7,26 +7,14 @@ This UI Framework uses the following libraries:
  - swfObject
  - BootstrapJS
 *****************************************************************************-*/
-var loader="def.ui.js";
+var loader="def.ui.bootstrap.js";
 var jsSrc = "http://def.nlx.org/framework/script/";
 var tagOpen = "<script src='"+jsSrc;
 var tagClose= "' type='text/javascript'></script>";
 var fileList = [
-    "code/jquery-1.6.4.min.js",
-    "code/jquery-ui.min.js",
-    "code/jquery.dataTables.min.js",
-    "code/jquery.mobile-1.0rc2.min.js",
-    "code/swfobject.js",
-    "code/bootstrap-alerts.js",
-    "code/bootstrap-buttons.js",
-    "code/bootstrap-dropdown.js",
-    "code/bootstrap-modal.js",
-    "code/bootstrap-popover.js",
-    "code/bootstrap-scrollspy.js",
-    "code/bootstrap-tabs.js",
-    "code/bootstrap-twipsy.js",
-    "code/foundation.js"
+    "code/bootstrap-dropdown.js"
 ];
+
 for(var i=0;i<fileList.length;i++){
     document.write(tagOpen+fileList[i]+tagClose)
 }
